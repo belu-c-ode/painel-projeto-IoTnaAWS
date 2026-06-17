b0da18ec5a444c8e97eec0452804b5580989b0e2
Colin Beluco <colin_40975@aluno.eseg.edu.br>
2026-06-17 13:57:00 -0300

    Atualizando arquivos do painel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  apiUrl = 'https://rtifx4axd1.execute-api.us-east-1.amazonaws.com/telemetry';
 
-  constructor() {}
+  dados: any[] = [];
+  ultimo: any = {};
+  chart: any;
 
+  async ngOnInit() {
+    await this.carregarDados();
+
+    setInterval(() => {
+      this.carregarDados();
+    }, 5000);
+  }
+
+  async carregarDados() {
+    const resposta = await fetch(this.apiUrl);
+    this.dados = await resposta.json();
+
+    this.ultimo = this.dados[this.dados.length - 1] || {};
+
+    setTimeout(() => this.criarGrafico(), 100);
+  }
+
+  criarGrafico() {
+    const labels = this.dados.map((_, i) => i + 1);
+    const temperaturas = this.dados.map(d => d.temperatura);
+    const distancias = this.dados.map(d => d.distancia);
+
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    this.chart = new Chart('grafico', {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: 'Temperatura',
+            data: temperaturas
+          },
+          {
+            label: 'Distância da água',
+            data: distancias
+          }
+        ]
+      }
+    });
+  }
 }
